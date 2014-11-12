85087e9ecded29e30ba53ecf5c3d8d62aea42771
pamod <pamod@live.com>
2014-11-12 15:24:39 +0530

    Adding licence headers, adding functionality to rollback during an exception occurs when adding the subscribers

--- modules/andes-core/broker/src/main/java/org/wso2/andes/mqtt/MQTTMessageMetaData.java
+++ modules/andes-core/broker/src/main/java/org/wso2/andes/mqtt/MQTTMessageMetaData.java
@@ -1,21 +1,20 @@
 /*
-*
-*  Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*  WSO2 Inc. licenses this file to you under the Apache License,
-*  Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ * Copyright (c) 2005-2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.wso2.andes.mqtt;
 
 import org.wso2.andes.server.store.MessageMetaDataType;
@@ -33,10 +32,15 @@ public class MQTTMessageMetaData implements StorableMessageMetaData {
 
     //Will store the following information as meta data of the message
     private long messageID;
+    //By default this will be true in the case of MQTT, since all the transactions are made through topics
     private boolean isTopic;
+    //The destination of the message, this will be the name of the topic
     private String destination;
+    //Should this be persisted or kept in memory
     private boolean isPersistance;
+    //The content length of the message
     private int messageLength;
+    //The level of qos, this can either be 0,1 or 2
     private int qosLevel;
 
 
