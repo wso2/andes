85d672162d6089d054e2545301bcbffa64d11665
pranavan <pranavan.11@cse.mrt.ac.lk>
2015-01-22 14:33:28 +0530

    Some Changes updated

--- modules/andes-core/broker/src/main/java/org/wso2/andes/subscription/SubscriptionBitArray.java
+++ modules/andes-core/broker/src/main/java/org/wso2/andes/subscription/SubscriptionBitArray.java
@@ -2,6 +2,25 @@ package org.wso2.andes.subscription;
 
 import java.util.BitSet;
 
+/*
+ * Copyright (c) 2005-2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
 /**
  * Represents the bit array which contain details of the mapping of criteria to
  * subscription
@@ -11,7 +30,7 @@ import java.util.BitSet;
  */
 public class SubscriptionBitArray {
     /**
-     * bit set representing a specific criteria Bit is set to 1 if a
+     * Bit set representing a specific criteria Bit is set to 1 if a
      * subscription is subscribed to a specific topic
      */
     private BitSet bitList;
@@ -71,18 +90,11 @@ public class SubscriptionBitArray {
         this.bitList = bitList;
     }
 
+    
     /**
-     * Method used to print the Bit array - for the checking purposes
+     * To get the bitList
+     *
      */
-    public void print() {
-        for (int i = 0; i < bitList.length(); i++)
-            if (bitList.get(i))
-                System.out.print("1 ");
-            else
-                System.out.print("0 ");
-        System.out.println();
-    }
-
     public BitSet getBits() {
         return bitList;
     }
