e2b68ae6590737f456e17abe0457b5779068f5e6
indunil.uom@gmail.com <indunil.uom@gmail.com>
2015-04-02 02:57:36 +0530

    remove unnecesssary imports and code lines

--- modules/andes-core/broker/src/main/java/org/wso2/andes/subscription/SubscriptionStore.java
+++ modules/andes-core/broker/src/main/java/org/wso2/andes/subscription/SubscriptionStore.java
@@ -34,20 +34,8 @@ import org.wso2.andes.kernel.SubscriptionAlreadyExistsException;
 import org.wso2.andes.kernel.SubscriptionListener.SubscriptionChange;
 import org.wso2.andes.mqtt.MQTTUtils;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class SubscriptionStore {
