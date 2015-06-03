114360374065873e570e0160b14bd31facd80682
anujalk <anush.online@gmail.com>
2015-06-03 11:10:18 +0530

    metrics renamed

--- modules/andes-core/broker/src/main/java/org/wso2/andes/matrics/MetricsConstants.java
+++ modules/andes-core/broker/src/main/java/org/wso2/andes/matrics/MetricsConstants.java
@@ -23,122 +23,121 @@ package org.wso2.andes.matrics;
  */
 public class MetricsConstants {
 
+    public static final String PREFIX = "org.wso2.mb.";
+
     /*DB OPERATIONS*/
-    //TODO use package structure
     /**
      * Time taken for database write operations
      */
-    public static final String DB_WRITE = "db_write";
+    public static final String DB_WRITE = PREFIX + "database.write";
 
     /**
      * Time taken for database read operations
      */
-    public static final String DB_READ = "db_read";
+    public static final String DB_READ = PREFIX + "database.read";
 
     /**
      * Add message content to the message store
      */
-    public static final String ADD_MESSAGE_PART = "db_store_message_part";
+    public static final String ADD_MESSAGE_PART = PREFIX + "store.messagePart.add";
     
     /**
      * Add message meta data list to the database
      */
-    public static final String ADD_META_DATA_LIST = "db_store_mata_data_list";
+    public static final String ADD_META_DATA_LIST = PREFIX + "store.metadataList.add";
     /**
      * Add message meta data to database
      */
-    public static final String ADD_META_DATA = "db_store_mata_data_list";
+    public static final String ADD_META_DATA = "store.metadata.add";
     
     /**
      * Add meta data to queue list
      */
-    public static final String ADD_META_DATA_TO_QUEUE_LIST = "db_store_mata_data_queue_list";
+    public static final String ADD_META_DATA_TO_QUEUE_LIST = "store.metadataToQueueList.add";
     /**
      * Add meta data to queue
      */
-    public static final String ADD_META_DATA_TO_QUEUE = "db.store.meta.data.queue";
+    public static final String ADD_META_DATA_TO_QUEUE = "store.metadataToQueue.add";
     /**
      * Add meta data as a batch
      */
-    public static final String ADD_META_DATA_TO_BATCH = "db_store_meta_data_batch";
+    public static final String ADD_META_DATA_TO_BATCH = "store.metadataToBatch.add";
     /**
      * Get message content as batch
      */
-    public static final String GET_CONTENT_BATCH = "db_get_content_as_batch";
+    public static final String GET_CONTENT_BATCH = "store.contentBatch.get";
     /**
      * Get message meta data
      */
-    public static final String GET_META_DATA = "db_get_mata_data";
+    public static final String GET_META_DATA = "store.metadata.get";
     /**
      * Get message meta data list
      */
-    public static final String GET_META_DATA_LIST = "db_get_mata_data_list";
+    public static final String GET_META_DATA_LIST = "store.metadataList.get";
     /**
      * Get message content
      */
-    public static final String GET_CONTENT = "db_get_content";
+    public static final String GET_CONTENT = "store.content.get";
     /**
      * Get next message meta data from queue
      */
-    public static final String GET_NEXT_MESSAGE_METADATA_FROM_QUEUE = "db_get_next_mata_data";
+    public static final String GET_NEXT_MESSAGE_METADATA_FROM_QUEUE = "store.nextMessageMetadataFromQueue.get";
     /**
      * Delete message part
      */
-    public static final String DELETE_MESSAGE_PART = "db_delete_message_part";
+    public static final String DELETE_MESSAGE_PART = "store.messagePart.delete";
     /**
      * Delete message meta data from queue
      */
-    public static final String DELETE_MESSAGE_META_DATA_FROM_QUEUE = "db_delete_message_meat_data_from_queue";
+    public static final String DELETE_MESSAGE_META_DATA_FROM_QUEUE = "store.messageMetadataFromQueue.delete";
     /**
      * Update meta data
      */
-    public static final String UPDATE_META_DATA_INFORMATION = "db_update_meta_info";
+    public static final String UPDATE_META_DATA_INFORMATION = "store.metadata.update";
 
     /*Buffer Values*/
 
     /**
      * At a given time the number of messages in the inbound disruptor ring
      */
-    public static final String DISRUPTOR_INBOUND_RING = "buffer_in_bound_ring_size";
+    public static final String DISRUPTOR_INBOUND_RING = "inbound.disruptor.message.count";
     /**
      * At a given time the number of messages which have being acknowledged in the inbound ring
      */
-    public static final String DISRUPTOR_MESSAGE_ACK = "buffer_message_ack_count";
+    public static final String DISRUPTOR_MESSAGE_ACK = "inbound.disruptor.ack.count";
     /**
      * At a given time the number of messages in the outbound ring
      */
-    public static final String DISRUPTOR_OUTBOUND_RING = "buffer_out_bound_ring";
+    public static final String DISRUPTOR_OUTBOUND_RING = PREFIX + "outbound.disruptor.message.count";
 
     /**
      * At a given time number of queue subscribers
      */
-    public static final String QUEUE_SUBSCRIBERS = "queue_subscribers";
+    public static final String QUEUE_SUBSCRIBERS = PREFIX + "queue.subscribers.count";
     /**
      * At a given time number of topic subscriber
      */
-    public static final String TOPIC_SUBSCRIBERS = "topic_subscribers";
-
-    /**
-     * Number of messages received per second. This metric is calculated when a message reaches server.
-     */
-    public static final String MSG_RECEIVE_RATE = "message_receive_rate";
-    /**
-     * Number of acknowledgments recieved from publishers per second.
-     */
-    public static final String ACK_RECEIVE_RATE = "ack_receive_rate";
-    /**
-     * Number of messages received per second. This metric is calculated when a message reaches server.
-     */
-    public static final String MSG_SENT_RATE = "message_receive_rate";
-    /**
-     * Number of acknowledgments recieved from publishers per second.
-     */
-    public static final String ACK_SENT_RATE = "ack_receive_rate";
-
-
+    public static final String TOPIC_SUBSCRIBERS = PREFIX + "topic.subscribers.count";
     /**
      * At a given time the number of active channels
      */
-    public static final String ACTIVE_CHANNELS = "active_channels";
+    public static final String ACTIVE_CHANNELS = PREFIX + "channels.active.count";
+
+    /**
+     * Number of messages received per second. This metric is calculated when a message reaches server.
+     */
+    public static final String MSG_RECEIVE_RATE = PREFIX + "message.receive";
+    /**
+     * Number of acknowledgments received from publishers per second.
+     */
+    public static final String ACK_RECEIVE_RATE = PREFIX + "ack.receive";
+    /**
+     * Number of messages sent per second. This metric is calculated when a message reaches server.
+     */
+    public static final String MSG_SENT_RATE = PREFIX + "message.sent";
+    /**
+     * Number of acknowledgments received from publishers per second.
+     */
+    public static final String ACK_SENT_RATE = PREFIX + "ack.sent";
 
 }
