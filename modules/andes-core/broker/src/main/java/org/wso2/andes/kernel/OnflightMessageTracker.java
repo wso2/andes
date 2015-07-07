092edc0244eda5d02ec2fc031ad21c263f0943f0
ramith <ramith@wso2.com>
2015-07-07 19:45:34 +0530

    Some fixes for MB-1148

--- modules/andes-core/broker/src/main/java/org/wso2/andes/kernel/OnflightMessageTracker.java
+++ modules/andes-core/broker/src/main/java/org/wso2/andes/kernel/OnflightMessageTracker.java
@@ -156,7 +156,7 @@ public class OnflightMessageTracker {
             this.timestamp = timestamp;
             this.expirationTime = expirationTime;
             this.channelToNumOfDeliveries = new ConcurrentHashMap<UUID, Integer>();
-            this.messageStatus = Collections.synchronizedList( new ArrayList<MessageStatus>());
+            this.messageStatus =  new ArrayList<MessageStatus>(); //  Collections.synchronizedList( new ArrayList<MessageStatus>());
             this.messageStatus.add(messageStatus);
             this.numberOfScheduledDeliveries = new AtomicInteger(0);
             this.arrivalTime = arrivalTime;
