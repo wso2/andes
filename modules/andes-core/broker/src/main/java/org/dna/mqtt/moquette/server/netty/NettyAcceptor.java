1ee29ba828058280f27fe02298db2b4fe5f190d5
pamod <pamod@live.com>
2015-07-22 18:58:41 +0530

    Refactoring SSL handler for MQTT

--- modules/andes-core/broker/src/main/java/org/dna/mqtt/moquette/server/netty/NettyAcceptor.java
+++ modules/andes-core/broker/src/main/java/org/dna/mqtt/moquette/server/netty/NettyAcceptor.java
@@ -28,24 +28,21 @@ import org.dna.mqtt.moquette.server.netty.metrics.MessageMetricsCollector;
 import org.dna.mqtt.moquette.server.netty.metrics.MessageMetricsHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.net.ssl.KeyManagerFactory;
+import org.wso2.andes.configuration.AndesConfigurationManager;
+import org.wso2.andes.configuration.enums.AndesConfiguration;
+import org.wso2.andes.transport.ConnectionSettings;
+import org.wso2.andes.transport.network.security.ssl.SSLUtil;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLEngine;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.security.KeyManagementException;
-import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
 import java.util.List;
 import java.util.Properties;
+import org.wso2.andes.configuration.modules.JKSStore;
 
 /**
  * @author andrea
@@ -292,45 +289,48 @@ public class NettyAcceptor implements ServerAcceptor {
         LOG.info(String.format("Bytes read: %d, bytes wrote: %d", bytesMetrics.readBytes(), bytesMetrics.wroteBytes()));
     }
 
+    /**
+     * Constructs connection settings required for ssl connectivity
+     *
+     * @param props holds information necessary to establish ssl connectivity
+     * @return ConnectionSettings
+     */
+    private ConnectionSettings constructConnectionSettings(Properties props) {
+        ConnectionSettings connectionSettings = new ConnectionSettings();
+        String certificateType = "SunX509";
 
+        String sslTrustStoreLocation = ((JKSStore) AndesConfigurationManager.readValue
+                (AndesConfiguration.TRANSPORTS_MQTT_SSL_CONNECTION_TRUSTSTORE)).getStoreLocation();
+        String sslTrustStorePassword = ((JKSStore) AndesConfigurationManager.readValue
+                (AndesConfiguration.TRANSPORTS_MQTT_SSL_CONNECTION_TRUSTSTORE)).getPassword();
+        String sslKeyStoreLocation = ((JKSStore) AndesConfigurationManager.readValue
+                (AndesConfiguration.TRANSPORTS_MQTT_SSL_CONNECTION_KEYSTORE)).getStoreLocation();
+        String sslKeyStorePassword = ((JKSStore) AndesConfigurationManager.readValue
+                (AndesConfiguration.TRANSPORTS_MQTT_SSL_CONNECTION_KEYSTORE)).getPassword();
+
+
+        connectionSettings.setTrustStorePath(sslTrustStoreLocation);
+        connectionSettings.setTrustStorePassword(sslTrustStorePassword);
+        connectionSettings.setTrustStoreCertType(certificateType);
+
+        connectionSettings.setKeyStorePath(sslKeyStoreLocation);
+        connectionSettings.setKeyStorePassword(sslKeyStorePassword);
+        connectionSettings.setKeyStoreCertType(certificateType);
+
+        return connectionSettings;
+    }
+
+
+    /**
+     * Initialization of SSLHandler
+     * @param props the configuration details
+     * @return
+     */
     private SslHandler initSSLHandler(Properties props) {
-        String jksPath = props.getProperty(Constants.JKS_PATH_PROPERTY_NAME);
-        //TODO remove the below hard coded
-        jksPath = "/Users/pamod/Desktop/dev/MQTTUpgrade/wso2mb-3.0.0-SNAPSHOT/repository/resources/security/wso2carbon.jks";
-        LOG.info("Starting SSL using keystore at {}", jksPath);
-        if (jksPath == null || jksPath.isEmpty()) {
-            //key_store_password or key_manager_password are empty
-            LOG.warn("You have configured the SSL port but not the jks_path, SSL not started");
-            return null;
-        }
-
-        //if we have the port also the jks then keyStorePassword and keyManagerPassword
-        //has to be defined
-        String keyStorePassword = props.getProperty(Constants.KEY_STORE_PASSWORD_PROPERTY_NAME);
-        String keyManagerPassword = props.getProperty(Constants.KEY_MANAGER_PASSWORD_PROPERTY_NAME);
-        //TODO remove the below
-        keyManagerPassword = "wso2carbon";
-        keyStorePassword = "wso2carbon";
-        if (keyStorePassword == null || keyStorePassword.isEmpty()) {
-            //key_store_password or key_manager_password are empty
-            LOG.warn("You have configured the SSL port but not the key_store_password, SSL not started");
-            return null;
-        }
-        if (keyManagerPassword == null || keyManagerPassword.isEmpty()) {
-            //key_manager_password or key_manager_password are empty
-            LOG.warn("You have configured the SSL port but not the key_manager_password, SSL not started");
-            return null;
-        }
 
         try {
-            InputStream jksInputStream = jksDatastore(jksPath);
-            SSLContext serverContext = SSLContext.getInstance("TLS");
-            final KeyStore ks = KeyStore.getInstance("JKS");
-            ks.load(jksInputStream, keyStorePassword.toCharArray());
-            final KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-            kmf.init(ks, keyManagerPassword.toCharArray());
-            serverContext.init(kmf.getKeyManagers(), null, null);
-
+            ConnectionSettings sslConnectionSettings = constructConnectionSettings(props);
+            SSLContext serverContext = SSLUtil.createSSLContext(sslConnectionSettings);
             SSLEngine engine = serverContext.createSSLEngine();
             engine.setUseClientMode(false);
             return new SslHandler(engine);
@@ -338,24 +338,12 @@ public class NettyAcceptor implements ServerAcceptor {
                 | KeyManagementException | IOException ex) {
             LOG.error("Can't start SSL layer!", ex);
             return null;
+        } catch (Exception e) {
+            //Here SSLUtils throws a generic type Exception, hence we need to catch it
+            //This is bad anyhow
+            LOG.error("Error while establishing ssl server context ", e.getMessage());
+            return null;
         }
     }
-
-    private InputStream jksDatastore(String jksPath) throws FileNotFoundException {
-        URL jksUrl = getClass().getClassLoader().getResource(jksPath);
-        if (jksUrl != null) {
-            LOG.info("Starting with jks at {}, jks normal {}", jksUrl.toExternalForm(), jksUrl);
-            return getClass().getClassLoader().getResourceAsStream(jksPath);
-        }
-        LOG.info("jks not found in bundled resources, try on the filesystem");
-        File jksFile = new File(jksPath);
-        if (jksFile.exists()) {
-            LOG.info("Using {} ", jksFile.getAbsolutePath());
-            return new FileInputStream(jksFile);
-        }
-        LOG.warn("File {} doesn't exists", jksFile.getAbsolutePath());
-        return null;
-    }
-
 
 }
