a6f68b752947aa937ec3f8c620fd4b55cf8d3807
Hemika Kodikara <hemika.kodikara@gmail.com>
2016-03-29 16:46:59 +0530

    Avoid loading keystores when not using SSL communication at the client.

--- modules/andes-core/client/src/main/java/org/wso2/andes/client/AMQConnectionDelegate_8_0.java
+++ modules/andes-core/client/src/main/java/org/wso2/andes/client/AMQConnectionDelegate_8_0.java
@@ -114,18 +114,15 @@ public class AMQConnectionDelegate_8_0 implements AMQConnectionDelegate
             settings.setCertAlias(sslConfig.getSslCertAlias());
             settings.setKeyStoreCertType(sslConfig.getCertType());
             settings.setTrustStoreCertType(sslConfig.getCertType());
+            if (settings.getKeyStorePath() == null && settings.getKeyStorePassword() == null &&
+                settings.getTrustStorePath() == null && settings.getTrustStorePassword() == null) {
+                _logger.warn("Loading keystore and truststore from system properties");
+                settings.loadSSLConfigFromSysConfig();
+            }
             if ((settings.getKeyStorePath() == null) || (settings.getKeyStorePassword() == null)) {
                 settings.setKeyStorePath(sslConfig.getTrustStorePath());
                 settings.setKeyStorePassword(sslConfig.getTrustStorePassword());
             }
-        }
-
-        if (settings.getKeyStorePath() == null && settings.getKeyStorePassword() == null && settings
-                .getTrustStorePath() == null && settings.getTrustStorePassword() == null) {
-
-            _logger.warn("Loading keystore and truststore from system properties");
-
-            settings.loadSSLConfigFromSysConfig();
         }
 
         OutgoingNetworkTransport transport = Transport.getOutgoingTransportInstance(getProtocolVersion());
