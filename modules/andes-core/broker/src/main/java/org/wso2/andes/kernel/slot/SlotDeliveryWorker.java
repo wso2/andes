77aa5ba1241324fbedd2b849b7ce0444277a0099
Hasitha Amal De Silva <hastef.ds@gmail.com>
2016-04-08 14:00:49 +0530

    [MB-1653] Stop SlotDeliveryWorkers when there are no active subscribers.

--- modules/andes-core/broker/src/main/java/org/wso2/andes/kernel/slot/SlotDeliveryWorker.java
+++ modules/andes-core/broker/src/main/java/org/wso2/andes/kernel/slot/SlotDeliveryWorker.java
@@ -227,6 +227,7 @@ public class SlotDeliveryWorker extends Thread implements StoreHealthListener{
             }
         }
 
+        log.info("SlotDeliveryWorker stopped. Thread name " + Thread.currentThread().getName() + " with Thread Id : " + this.getId());
     }
 
     /**
@@ -276,6 +277,14 @@ public class SlotDeliveryWorker extends Thread implements StoreHealthListener{
                 }
             }
         }
+
+        if (0 == storageQueueDataMap.size()) {
+            if (log.isDebugEnabled()) {
+                log.debug("Stopping SlotDeliveryWorker : " + this.getId() + " due to 0 subscribers on its queues. ");
+            }
+            this.setRunning(false);
+        }
+
     }
 
     /**
