e3aa7c758125576d287792dec78f295af3af67ac
Indika Sampath <indikas@wso2.com>
2016-07-22 09:20:47 +0530

    counter stats added for enqueue, dequeue, acknowledge and reject

--- modules/andes-core/broker/src/main/java/org/wso2/andes/metrics/MetricsConstants.java
+++ modules/andes-core/broker/src/main/java/org/wso2/andes/metrics/MetricsConstants.java
@@ -57,30 +57,37 @@ public class MetricsConstants {
      * Add meta data to queue list
      */
     public static final String ADD_META_DATA_TO_QUEUE_LIST = PREFIX + "store.metadataToQueueList.add";
+
     /**
      * Add meta data to queue
      */
     public static final String ADD_META_DATA_TO_QUEUE = PREFIX + "store.metadataToQueue.add";
+
     /**
      * Add meta data as a batch
      */
     public static final String ADD_META_DATA_TO_BATCH = PREFIX + "store.metadataToBatch.add";
+
     /**
      * Get message content as batch
      */
     public static final String GET_CONTENT_BATCH = PREFIX + "store.contentBatch.get";
+
     /**
      * Get message meta data
      */
     public static final String GET_META_DATA = PREFIX + "store.metadata.get";
+
     /**
      * Get message meta data list
      */
     public static final String GET_META_DATA_LIST = PREFIX + "store.metadataList.get";
+
     /**
      * Get message meta data list in DLC for a queue
      */
     public static final String GET_META_DATA_LIST_FOR_QUEUE_FROM_DLC = PREFIX + "store.metadataListForQueueFromDLC.get";
+
     /**
      * Get message content
      */
@@ -95,6 +102,7 @@ public class MetricsConstants {
      * Get next message meta data from queue
      */
     public static final String GET_NEXT_MESSAGE_METADATA_FROM_QUEUE = PREFIX + "store.nextMessageMetadataFromQueue.get";
+
     /**
      * Get next message meta data in dead letter channel for queue
      */
@@ -104,14 +112,17 @@ public class MetricsConstants {
      * Get next message meta data in dead letter channel
      */
     public static final String GET_NEXT_MESSAGE_METADATA_IN_DLC = PREFIX + "store.nextMessageMetadataInDLC.get";
+
     /**
      * Move message metadata to DLC
      */
     public static final String MOVE_METADATA_TO_DLC = PREFIX + "store.messageMetadataToDLC.Move";
+
     /**
      * Delete message meta data from queue
      */
     public static final String DELETE_MESSAGE_META_DATA_FROM_QUEUE = PREFIX + "store.messageMetadataFromQueue.delete";
+
     /**
      * Delete metadata and content
      */
@@ -133,10 +144,12 @@ public class MetricsConstants {
      * At a given time the number of messages in the inbound disruptor ring
      */
     public static final String DISRUPTOR_INBOUND_RING = PREFIX + "inbound.disruptor.message.count";
+
     /**
      * At a given time the number of messages which have being acknowledged in the inbound ring
      */
     public static final String DISRUPTOR_MESSAGE_ACK = PREFIX + "inbound.disruptor.ack.count";
+
     /**
      * At a given time the number of messages in the outbound ring
      */
@@ -146,10 +159,12 @@ public class MetricsConstants {
      * At a given time number of queue subscribers
      */
     public static final String QUEUE_SUBSCRIBERS = PREFIX + "queue.subscribers.count";
+
     /**
      * At a given time number of topic subscriber
      */
     public static final String TOPIC_SUBSCRIBERS = PREFIX + "topic.subscribers.count";
+
     /**
      * At a given time the number of active channels
      */
@@ -159,17 +174,40 @@ public class MetricsConstants {
      * Number of messages received per second. This metric is calculated when a message reaches server.
      */
     public static final String MSG_RECEIVE_RATE = PREFIX + "message.receive";
+
     /**
      * Number of acknowledgments received from publishers per second.
      */
     public static final String ACK_RECEIVE_RATE = PREFIX + "ack.receive";
+
     /**
      * Number of messages sent per second. This metric is calculated when a message reaches server.
      */
     public static final String MSG_SENT_RATE = PREFIX + "message.sent";
+
     /**
      * Number of acknowledgments received from publishers per second.
      */
     public static final String ACK_SENT_RATE = PREFIX + "ack.sent";
+
+    /**
+     * Number of messages received to broker
+     */
+    public static final String ENQUEUE_MESSAGES = PREFIX + "enqueue.count";
+
+    /**
+     * Number of messages delivered to consumer
+     */
+    public static final String DEQUEUE_MESSAGES = PREFIX + "dequeue.count";
+
+    /**
+     * Number of messages delivered to consumer
+     */
+    public static final String ACK_MESSAGES = PREFIX + "ack.count";
+
+    /**
+     * Number of messages delivered to consumer
+     */
+    public static final String REJECT_MESSAGES = PREFIX + "reject.count";
 
 }
